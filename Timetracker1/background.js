2e991a71e4ea965ce9b472fb0152465f2c72e37c
Chandramani Vyas <chandramani09022001@gmail.com>
2024-08-27 15:56:51 +0530

    Final update

--- Timetracker1/background.js
+++
@@ -1,6 +0,0 @@
-chrome.action.onClicked.addListener(function (tab) {
-  const currentTotal = localStorage.getItem("currentTotal") || 0;
-  chrome.storage.local.set({ currentTotal: currentTotal }, function () {
-    chrome.tabs.create({ url: "http://127.0.0.1:5500/weekly.html" });
-  });
-});
